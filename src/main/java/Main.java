4a03a4973bdddc2128a5d17743b4285648482642
Eldus <arsik2001@gmail.com>
2021-02-23 18:08:43 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,50 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+
+
+public class Main {
+
+    public static void task2(Object o) throws IllegalAccessException {
+        Field[] fields = o.getClass().getDeclaredFields();
+
+        for(Field field : fields){
+            if(!field.isAnnotationPresent(Ignore.class)){
+                StringBuilder stringBuilder = new StringBuilder();
+                field.setAccessible(true);
+                stringBuilder.append(Modifier.toString(field.getModifiers()));
+                stringBuilder.append('|');
+                stringBuilder.append(field.getName());
+                stringBuilder.append('|');
+                stringBuilder.append(field.get(o));
+                System.out.println(stringBuilder.toString());
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        String s = "hello";
+
+//        s = String.class
+//                .getConstructor(String.class)
+//                .newInstance("ABCDE");
+//        System.out.println(s);
+
+
+        Field field = String.class.getDeclaredField("value");
+        field.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, Modifier.PUBLIC);
+        byte[] value = {'A','B','C','D','E'};
+        field.set(s,value);
+        System.out.println(s);
+
+        task2(new Test(new char[]{'A','B','C','D','E'}));
+
+
+
+    }
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+    @Ignore
+    private final char[] k;
+    private int age = 13;
+    protected String name = "Nikita";
+    public int iq;
+
+    public Test(char[] k) {
+        this.k = k;
+    }
+
+    public char[] getK() {
+        return k;
+    }
+}
