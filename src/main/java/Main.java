875faae69d91cb15aa99fad7a9341db25e463f42
Eldus <arsik2001@gmail.com>
2021-02-23 18:17:31 +0200

    refactor commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void task2(Object o) throws IllegalAccessException {
         Field[] fields = o.getClass().getDeclaredFields();
 
-        for(Field field : fields){
-            if(!field.isAnnotationPresent(Ignore.class)){
+        for (Field field : fields) {
+            if (!field.isAnnotationPresent(Ignore.class)) {
                 StringBuilder stringBuilder = new StringBuilder();
                 field.setAccessible(true);
                 stringBuilder.append(Modifier.toString(field.getModifiers()));
@@ -24,26 +24,28 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String s = "hello";
 
-//        s = String.class
-//                .getConstructor(String.class)
-//                .newInstance("ABCDE");
-//        System.out.println(s);
+        s = String.class
+                .getConstructor(String.class)
+                .newInstance("EDCBA");
+        System.out.println(s);
 
 
         Field field = String.class.getDeclaredField("value");
         field.setAccessible(true);
+
+        // Из за этой строчки и пришлось даунгрейднуться с 13 в 9 , выдавало java.lang.NoSuchFieldException: modifiers, хотя очевидно что такое поле там было)
         Field modifiersField = Field.class.getDeclaredField("modifiers");
+
         modifiersField.setAccessible(true);
         modifiersField.setInt(field, Modifier.PUBLIC);
-        byte[] value = {'A','B','C','D','E'};
-        field.set(s,value);
+        byte[] value = {'A', 'B', 'C', 'D', 'E'};
+        field.set(s, value);
         System.out.println(s);
 
-        task2(new Test(new char[]{'A','B','C','D','E'}));
-
+        task2(new Test(new char[]{'A', 'B', 'C', 'D', 'E'}));
 
 
     }
